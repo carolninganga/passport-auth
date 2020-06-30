cb96fce0447dae587889e997a1f589a659d5fd3c
Caroline Ninganga <carolninganga@gmail.com>
2020-06-30 19:27:45 -0400

    added welcome form to app.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res) => res.send('Welcome'));
+router.get('/',(req, res) => { 
+    res.sendFile(path.join(__dirname, '../client/public/index.html'))
+});
+
 
 module.exports = router;
