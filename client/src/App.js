25f774aa4813100a21e7f21b1a143d058f7b0f68
Caroline Ninganga <carolninganga@gmail.com>
2020-06-30 20:39:53 -0400

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Form from './component/Form/Form';
+import Form from './pages/Form';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <Form />
-    
-    </div>
+    <BrowserRouter>
+      <Switch>
+      <Route exact path='/' component={Form} />
+      <Route exact path='/users/login' component={Login} />
+      <Route exact path='/users/register' component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
